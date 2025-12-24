afe0f123476f973fa56d6acd5e22828f63246577
Palak khare <palakkhare1902@gmail.com>
2025-12-24 11:29:42 +0530

    Implement read functionality for binary file
    Added functionality to read from a binary file after writing data to it.

--- file handling/binary_file.py
+++ file handling/binary_file.py
@@ -4,3 +4,8 @@
 data = b'\x00\x01\x02\x03\x04'
 with open('example.bin', 'wb') as file:
     file.write(data)
+
+# Reading a binary file
+with open('example.bin', 'rb') as file:
+    content = file.read()
+    print(content) # output b'\x00\x01\x02\x03\x04'
