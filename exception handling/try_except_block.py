15a6f629ff18eafe1af986b7c832e4e7d3e0d11c
Palak khare <palakkhare1902@gmail.com>
2026-01-10 12:37:48 +0000

    try except block

---
+++ exception handling/try_except_block.py
@@ -0,0 +1,24 @@
+## Exception try ,except block
+
+try:
+    a=b
+except:
+    print("The variable has not been assigned")
+
+    # output The variable has not been assigned
+
+## try,except,else and finally
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+except ValueError:
+    print("That's not a valid number!")
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except Exception as ex:
+    print(ex)
+else:
+    print(f"The result is {result}")
+finally:
+    print("Execution complete.")
+
